ff93243964e67adae29bd5b32b6c2f5de0ea6a18
Randall Johnson <rj9317@gmail.com>
2026-05-04 09:54:12 -0600

    feat: default name to cluster-name in control-plane-only

--- pkg/cli/v0/genesis_control_plane.go
+++ pkg/cli/v0/genesis_control_plane.go
@@ -1225,6 +1225,11 @@ func ValidateCreateGenesisControlPlaneFlags(
 	controlPlaneOnly bool,
 	clusterName string,
 ) error {
+	// ensure name is provided
+	if instanceName == "" {
+		return errors.New("--name is required (or --cluster-name with --control-plane-only)")
+	}
+
 	// ensure name length doesn't exceed maximum
 	if utf8.RuneCountInString(instanceName) > threeport.InstanceNameMaxLength {
 		return fmt.Errorf(

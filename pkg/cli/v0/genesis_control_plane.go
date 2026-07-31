59405e6b180606ee79a96bca9aba8fa3cd0222a3
Randy Johnson <rj9317@gmail.com>
2026-07-31 19:11:57 +0000

    fix: use created runtime id on control plane instance

--- pkg/cli/v0/genesis_control_plane.go
+++ pkg/cli/v0/genesis_control_plane.go
@@ -920,7 +920,7 @@ func CreateGenesisControlPlane(customInstaller *threeport.ControlPlaneInstaller)
 			Reconciled: &reconciled,
 		},
 		Namespace:                   &cpi.Opts.Namespace,
-		KubernetesRuntimeInstanceID: kubernetesRuntimeInstance.ID,
+		KubernetesRuntimeInstanceID: kubernetesRuntimeInstResult.ID,
 		Genesis:                     &genesis,
 		IsSelf:                      &selfInstance,
 		ApiServerEndpoint:           &threeportAPIEndpoint,

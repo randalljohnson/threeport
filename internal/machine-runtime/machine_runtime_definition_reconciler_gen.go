d40f78403b86c3b62f683a8fd9f4cd3b0a708fc7
Randy Johnson <rj9317@gmail.com>
2026-08-21 16:21:19 +0000

    chore: regen

--- internal/machine-runtime/machine_runtime_definition_reconciler_gen.go
+++ internal/machine-runtime/machine_runtime_definition_reconciler_gen.go
@@ -198,7 +198,7 @@ func MachineRuntimeDefinitionReconciler(r *controller.Reconciler) {
 					continue
 				}
 				if customRequeueDelay != 0 {
-					log.V(1).Info("create requeued for future reconciliation")
+					log.Info("create requeued for future reconciliation")
 					r.UnlockAndRequeue(
 						machineRuntimeDefinition,
 						customRequeueDelay,
@@ -245,7 +245,7 @@ func MachineRuntimeDefinitionReconciler(r *controller.Reconciler) {
 					continue
 				}
 				if customRequeueDelay != 0 {
-					log.V(1).Info("update requeued for future reconciliation")
+					log.Info("update requeued for future reconciliation")
 					r.UnlockAndRequeue(
 						machineRuntimeDefinition,
 						customRequeueDelay,
@@ -270,19 +270,6 @@ func MachineRuntimeDefinitionReconciler(r *controller.Reconciler) {
 					operationErr = errors.New("unrecognized version of machine runtime definition encountered for creation")
 				}
 				if operationErr != nil {
-					if errors.Is(operationErr, tpclient_lib.ErrConflict) {
-						log.Info(
-							"conflict reconciling deleted machine runtime definition object, requeueing",
-							"cause", operationErr.Error(),
-						)
-						r.UnlockAndRequeue(
-							machineRuntimeDefinition,
-							int64(30),
-							lockReleased,
-							msg,
-						)
-						continue
-					}
 					errorMsg := "failed to reconcile deleted machine runtime definition object"
 					log.Error(operationErr, errorMsg)
 					r.EventsRecorder.HandleEventOverride(
@@ -305,7 +292,7 @@ func MachineRuntimeDefinitionReconciler(r *controller.Reconciler) {
 					continue
 				}
 				if customRequeueDelay != 0 {
-					log.V(1).Info("delete requeued for future reconciliation")
+					log.Info("delete requeued for future reconciliation")
 					r.UnlockAndRequeue(
 						machineRuntimeDefinition,
 						customRequeueDelay,
@@ -339,19 +326,6 @@ func MachineRuntimeDefinitionReconciler(r *controller.Reconciler) {
 					machineRuntimeDefinition.GetId(),
 				)
 				if err != nil {
-					if errors.Is(err, tpclient_lib.ErrConflict) {
-						log.Info(
-							"conflict deleting machine runtime definition, requeueing",
-							"cause", err.Error(),
-						)
-						r.UnlockAndRequeue(
-							machineRuntimeDefinition,
-							int64(30),
-							lockReleased,
-							msg,
-						)
-						continue
-					}
 					log.Error(err, "failed to delete machine runtime definition")
 					r.UnlockAndRequeue(machineRuntimeDefinition, requeueDelay, lockReleased, msg)
 					continue

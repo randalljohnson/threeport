7cedbc48883c84d49c345de05acc3275ab210929
Randy Johnson <rj9317@gmail.com>
2026-09-09 16:04:40 +0000

    chore: regen

--- pkg/api-server/v0/handlers/gcp_gen.go
+++ pkg/api-server/v0/handlers/gcp_gen.go
@@ -110,7 +110,6 @@ func (h Handler) AddGcpGceMachineRuntimeDefinition(c echo.Context) error {
 // @Failure 500 {object} v0.Response "Internal Server Error"
 // @Router /v0/gcp-gce-machine-runtime-definitions [GET]
 func (h Handler) GetGcpGceMachineRuntimeDefinitions(c echo.Context) error {
-	objectType := api_v0.ObjectTypeGcpGceMachineRuntimeDefinition
 	fullyQualifiedType := new(api_v0.GcpGceMachineRuntimeDefinition).GetFullyQualifiedType()
 
 	// get pagination parameters
@@ -605,7 +604,6 @@ func (h Handler) AddGcpGceMachineRuntimeInstance(c echo.Context) error {
 // @Failure 500 {object} v0.Response "Internal Server Error"
 // @Router /v0/gcp-gce-machine-runtime-instances [GET]
 func (h Handler) GetGcpGceMachineRuntimeInstances(c echo.Context) error {
-	objectType := api_v0.ObjectTypeGcpGceMachineRuntimeInstance
 	fullyQualifiedType := new(api_v0.GcpGceMachineRuntimeInstance).GetFullyQualifiedType()
 
 	// get pagination parameters

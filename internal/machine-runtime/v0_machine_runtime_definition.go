b4008351fa6a85bee9a1fe9b3ecc3e8788661bfb
Randy Johnson <rj9317@gmail.com>
2026-09-15 18:58:16 -0600

    fix: import machine mapping from pkg mapping

--- internal/machine-runtime/v0_machine_runtime_definition.go
+++ internal/machine-runtime/v0_machine_runtime_definition.go
@@ -8,10 +8,10 @@ import (
 
 	logr "github.com/go-logr/logr"
 
-	"github.com/threeport/threeport/internal/kubernetes-runtime/mapping"
 	v0 "github.com/threeport/threeport/pkg/api/v0"
 	client "github.com/threeport/threeport/pkg/client/v0"
 	controller "github.com/threeport/threeport/pkg/controller/v0"
+	mapping "github.com/threeport/threeport/pkg/mapping/v0"
 	util "github.com/threeport/threeport/pkg/util/v0"
 )
 
--- internal/machine-runtime/v0_machine_runtime_instance.go
+++ internal/machine-runtime/v0_machine_runtime_instance.go
@@ -10,12 +10,12 @@ import (
 	logr "github.com/go-logr/logr"
 	"golang.org/x/crypto/ssh"
 
-	"github.com/threeport/threeport/internal/kubernetes-runtime/mapping"
 	v0 "github.com/threeport/threeport/pkg/api/v0"
 	client "github.com/threeport/threeport/pkg/client/v0"
 	controller "github.com/threeport/threeport/pkg/controller/v0"
 	event "github.com/threeport/threeport/pkg/event/v0"
 	machine "github.com/threeport/threeport/pkg/machine/v0"
+	mapping "github.com/threeport/threeport/pkg/mapping/v0"
 	util "github.com/threeport/threeport/pkg/util/v0"
 )
 

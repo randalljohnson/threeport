5732a24dc6710315b9485a37ea658efaf2d39848
Randy Johnson <rj9317@gmail.com>
2026-09-22 04:31:22 -0600

    fix: drop leftover merge marker in genesis tests

--- pkg/cli/v0/genesis_control_plane_test.go
+++ pkg/cli/v0/genesis_control_plane_test.go
@@ -12,7 +12,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-<<<<<<< HEAD
 	apiserver_lib "github.com/threeport/threeport/pkg/api-server/lib/v0"
 	v0 "github.com/threeport/threeport/pkg/api/v0"
 	threeport "github.com/threeport/threeport/pkg/threeport-installer/v0"
